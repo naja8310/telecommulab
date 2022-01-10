463246c3f9fe2a2121dbe3d80b5c20b50e8d708a
naja8310 <lnwkir@gmail.com>
2022-01-11 00:22:33 +0700

    11/1
    Signed-off-by: naja8310 <lnwkir@gmail.com>

---
+++ TelComSys2DSLab1/1.2.py
@@ -0,0 +1,7 @@
+# openCV use BGR (RGB color space)
+import cv2
+import sys
+import numpy 
+path = 'D:\Lab\Telecommunication Lab\TelComSys2DSLab1\test.jpg'
+img = cv2.imread(path,0)
+print(img)
--- TelComSys2DSLab1/lab1.py
+++
@@ -1,7 +0,0 @@
-import cv2
-from matplotlib import pyplot as plt
-
-img = cv2.imread('D:\Lab\Telecommunication Lab\TelComSys2DSLab1\CGFaceGrayscale.bmp',0)
-
-plt.imshow(img, cmap='gray')
-plt.show()
