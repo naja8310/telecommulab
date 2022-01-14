70f5fd4935d9f6c40e05338ba35026f0be872464
naja8310 <lnwkir@gmail.com>
2022-01-14 17:10:45 +0700

    .
    Signed-off-by: naja8310 <lnwkir@gmail.com>

--- lab1/1.2.py
+++ lab1/1.2.py
@@ -2,6 +2,6 @@
 import cv2
 import sys
 import numpy 
-path = 'D:\Lab\Telecommunication Lab\TelComSys2DSLab1\test.jpg'
+path = 'D:\Lab\Telecommunication Lab\TelComSys2DSLab1\imgtst.jpg'
 img = cv2.imread(path,0)
 print(img)
