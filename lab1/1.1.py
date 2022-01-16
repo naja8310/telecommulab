5a7ce5c757f9ea3efec4e13e24d39bc28a698431
naja8310 <lnwkir@gmail.com>
2022-01-16 23:41:22 +0700

    lab1
    Signed-off-by: naja8310 <lnwkir@gmail.com>

--- lab1/1.1.py
+++
@@ -1,7 +0,0 @@
-# openCV use BGR (RGB color space)
-import cv2
-from matplotlib import pyplot as plt
-path = 'D:\Lab\Telecommunication Lab\TelComSys2DSLab1\CGFaceGrayscale.bmp'
-img = cv2.imread(path,0)
-plt.imshow(img,cmap='gray')
-plt.show()
--- lab1/1.2.py
+++
@@ -1,7 +0,0 @@
-# openCV use BGR (RGB color space)
-import cv2
-import sys
-import numpy 
-path = 'D:\Lab\Telecommunication Lab\TelComSys2DSLab1\imgtst.jpg'
-img = cv2.imread(path,0)
-print(img)
