8b6655eb337c86af5e8211746d65aa16321fa0c3
naja8310 <lnwkir@gmail.com>
2022-01-24 14:06:23 +0700

    lab2
    Signed-off-by: naja8310 <lnwkir@gmail.com>

--- lab2/self_hist.py
+++ lab2/self_hist.py
@@ -4,10 +4,10 @@ img = cv2.imread('D:\Lab\Telecommunication Lab\lab2\self.bmp',0)
 compress = cv2.imread('D:\Lab\Telecommunication Lab\lab2\MyCompressedFace.jpeg',0)
 #display
 plt.subplot(1,2,1) # 1 row 2 colum img 1
-plt.hist(img.ravel(),256,[0,256])
+plt.hist(img.ravel(),254,[0,254])
 plt.title('Original img')
 plt.subplot(1,2,2) # 1 row 2 colum img 2
-plt.hist(compress.ravel(),256,[0,256])
+plt.hist(compress.ravel(),253,[0,253])
 plt.title('Compressed img')
 plt.suptitle('Settasak Images')
 plt.show()
