9d865b5392cb963c670ecdee2815cae3f766a24e
naja8310 <lnwkir@gmail.com>
2022-02-08 18:06:37 +0700

    lab4done
    Signed-off-by: naja8310 <lnwkir@gmail.com>

--- lab4/thresholdminmax.py
+++ lab4/thresholdminmax.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 img = cv2.imread(r'D:\Lab\Telecommunication Lab\lab4\MyFacePicSVRC.jpg',0)
 blur = cv2.GaussianBlur(img,(5,5),0) 
-mask = cv2.inRange(blur,160,205)
+mask = cv2.inRange(blur,160,190)
 ret,th = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 plt.subplot(121)
 plt.imshow(img,cmap='gray')
